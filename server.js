7e97dbaea08e9e6e87ad2eecccdb9b5dfd1cf3a1
youssef-saidi <78275450+youssef-saidi@users.noreply.github.com>
2022-04-10 23:20:47 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require('express')
+ const HouseRoute=require('./routes/HouseRoute')
+
+
+const app=express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Methods','*')
+    res.setHeader('Access-Control-Allow-Headers','Content-Type')
+    next()
+})
+
+ app.use('/',HouseRoute)
+
+
+
+app.listen(5000,()=>{
+    console.log("server run port :: 5000")
+})
